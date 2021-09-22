0e940d54ea3d90ddf940a81bf76761f76da7e8a2
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-09-21 18:54:12 -0600

    Correccion Usuario
    Correccion en la clase entitie usuario

--- api/municipalidad/src/main/java/org/una/municipalidad/entities/Usuario.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/entities/Usuario.java
@@ -1,11 +1,18 @@
 package org.una.municipalidad.entities;
 
-import lombok.AccessLevel;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
 
 public class Usuario implements Serializable {
     @Id
