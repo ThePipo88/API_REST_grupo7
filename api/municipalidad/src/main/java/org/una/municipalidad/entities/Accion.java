c0047f345e71e7bc8031c9e466349acf2ab5e3b4
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-09-21 18:44:28 -0600

    Revert "Revert "Merge branch 'main' into Michael_Solis""
    This reverts commit 78add536613de2a5daaa131de35d79d56f283a77.

---
+++ api/municipalidad/src/main/java/org/una/municipalidad/entities/Accion.java
@@ -0,0 +1,38 @@
+package org.una.municipalidad.entities;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "acciones")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Accion implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="usuarios_id")
+    private Usuario usuario;
+
+    @Column(name = "objeto", length = 45)
+    private String objeto;
+
+    @Column(name = "accion", length = 45)
+    private String accion;
+
+    @Column(name = "fecha_creacion", updatable = false)
+    @Temporal(TemporalType.DATE)
+    @Setter(AccessLevel.NONE)
+    private Date fechaCreacion;
+}
