415a181d951457fc16c65f2ba6f284b7c0050946
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-09-22 22:24:24 -0600

    Creacion Entidad Limpieza de Vias

---
+++ api/municipalidad/src/main/java/org/una/municipalidad/entities/Limpieza_Via.java
@@ -0,0 +1,43 @@
+package org.una.municipalidad.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "limpieza_vias")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+
+public class Limpieza_Via implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "provincia", length = 100)
+    private String provincia;
+
+    @Column(name = "canton", length = 100)
+    private String canton;
+
+    @Column(name = "distrito", length = 100)
+    private String distrito;
+
+    @Column(name = "direccion", length = 100)
+    private String direccion;
+
+    @Column(name = "geolocalizacion", length = 100)
+    private String geolocalizacion;
+
+    @Column(name = "zona", length = 100)
+    private String zona;
+
+    @Column(name = "metros_frente", length = 100)
+    private String metrosFrente;
+
+    private static final long serialVersionUID = 1L;
+}
