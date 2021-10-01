f7ca10ccfbbf3487d9826ddac4499056f5e30230
Reyner Badilla Arias <reyner.badilla.arias@est.una.ac.cr>
2021-09-30 21:39:21 -0600

    Se agregaron los DTO de Excepcion,Propiedad,Recibo y Servicio

---
+++ api/municipalidad/src/main/java/org/una/municipalidad/dto/ServicioDTO.java
@@ -0,0 +1,28 @@
+package org.una.municipalidad.dto;
+
+import lombok.*;
+import org.una.municipalidad.entities.ContribuyenteServicio;
+import org.una.municipalidad.entities.Propiedad;
+import org.una.municipalidad.entities.RutaBus;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ServicioDTO {
+
+    private Long id;
+    private String tipoServicio;
+    private String descripcion;
+    private String estado;
+    private Propiedad propiedad;
+    private Date fechaRegistro;
+    private Date ultimaActualizacion;
+
+}
