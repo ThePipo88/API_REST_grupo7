b72879f35b90f5d00fb5fbad08e28662c736e4ab
Reyner Badilla Arias <reyner.badilla.arias@est.una.ac.cr>
2021-09-30 21:44:33 -0600

    DTO Reyner y Maikol

---
+++ api/municipalidad/src/main/java/org/una/municipalidad/dto/ListaSalidaDTO.java
@@ -0,0 +1,18 @@
+package org.una.municipalidad.dto;
+
+import lombok.*;
+import org.una.municipalidad.entities.RutaBus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ListaSalidaDTO {
+
+    private Long id;
+    private RutaBus rutaBus;
+    private String dia;
+    private int cantidad;
+
+}
