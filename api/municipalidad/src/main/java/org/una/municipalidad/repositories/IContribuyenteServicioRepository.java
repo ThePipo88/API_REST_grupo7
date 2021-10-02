4e0cd7c2998d28b365c3c9fe406239bb065024f8
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-10-01 20:17:18 -0600

    Creacion IContribuyenteServicioRepository

--- api/municipalidad/src/main/java/org/una/municipalidad/repositories/IContribuyenteServicioRepository.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/repositories/IContribuyenteServicioRepository.java
@@ -1,4 +1,17 @@
 package org.una.municipalidad.repositories;
 
-public interface IContribuyenteServicioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.una.municipalidad.entities.ContribuyenteServicio;
+
+
+import java.util.List;
+
+public interface IContribuyenteServicioRepository extends JpaRepository<ContribuyenteServicio, Long> {
+
+    public List<ContribuyenteServicio> findByIdContribuyenteServicio(Long id);
+
+    public List<ContribuyenteServicio> findByPorcentajeServicio(String porcentaje);
+
+
+
 }
