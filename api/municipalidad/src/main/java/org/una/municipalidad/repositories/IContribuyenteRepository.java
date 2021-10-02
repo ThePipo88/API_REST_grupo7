14f116ccab26b0c987cf97b96641cb1c13177c69
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-10-01 20:48:29 -0600

    Modificacion en IContribuyenteRepository

--- api/municipalidad/src/main/java/org/una/municipalidad/repositories/IContribuyenteRepository.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/repositories/IContribuyenteRepository.java
@@ -4,16 +4,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.una.municipalidad.entities.Contribuyente;
 
 
-
+import java.util.Date;
 import java.util.List;
 
 
 public interface IContribuyenteRepository extends JpaRepository<Contribuyente, Long> {
 
-    public Contribuyente findByCedula(String cedula);
+    public Contribuyente findById(String id);
 
-    public List<Contribuyente> findByCedulaContaining(String cedula);
+    public List<Contribuyente> findByIdContaining(String id);
 
-    public List<Contribuyente> findByNombreCompletoContainingIgnoreCase(String nombreCompleto);
+    public List<Contribuyente> findByFechaNacimientoContainingIgnoreCase(Date fechaNacimiento);
+
+    public List<Contribuyente> findByDireccionContainingIgnoreCase(String direccion);
+
+    public List<Contribuyente> findByCorreoElectronicoContainingIgnoreCase(String correoElectronico);
+
+    public List<Contribuyente> findByTelefonoContainingIgnoreCase(String telefono);
+
 
 }
