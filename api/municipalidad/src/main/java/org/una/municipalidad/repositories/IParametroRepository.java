86bc72f2c2308736ae055a0db1a6e346ca7bdccc
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-10-01 20:27:13 -0600

    Creacion de IParametroRepository

--- api/municipalidad/src/main/java/org/una/municipalidad/repositories/IParametroRepository.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/repositories/IParametroRepository.java
@@ -2,7 +2,16 @@ package org.una.municipalidad.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.una.municipalidad.entities.Parametro;
+import org.una.municipalidad.entities.Recibo;
+
+import java.util.Date;
+import java.util.List;
 
 public interface IParametroRepository extends JpaRepository<Parametro, Long> {
 
+    public List<Parametro> findByNombre(String nombre);
+
+    public List<Parametro> findByFormula(String formula);
+
+    public List<Parametro> findByEstado(boolean estado);
 }
