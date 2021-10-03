a70bfa09e3316ea083e8c04da9d9349f6ddd289f
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2021-10-02 18:49:23 -0600

    Cracion de ICobroGeneradoService, IContribuyenteServicioService, IContribuyenteService, y IParametroService

---
+++ api/municipalidad/src/main/java/org/una/municipalidad/services/ICobroGeneradoService.java
@@ -0,0 +1,28 @@
+package org.una.municipalidad.services;
+
+import org.una.municipalidad.dto.CobroGeneradoDTO;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface ICobroGeneradoService {
+
+    public Optional<List<CobroGeneradoDTO>> findAll();
+
+    public Optional<CobroGeneradoDTO> findById(Long id);
+
+    public Optional<CobroGeneradoDTO> findByMonto(Double monto);
+
+    public Optional<List<CobroGeneradoDTO>> findByObjetoAndFechaCobroBetween(String objetoId, Date startDate, Date endDate);
+
+    public Optional<List<CobroGeneradoDTO>> findByFechaCobroBetween(Date startDate, Date endDate);
+
+    public Optional<CobroGeneradoDTO> create(CobroGeneradoDTO cobroGeneradoDTO);
+
+    public Optional<CobroGeneradoDTO> update(CobroGeneradoDTO cobroGeneradoDTO, Long id);
+
+    public void delete(Long id);
+
+    public void deleteAll();
+}
---
+++ api/municipalidad/src/main/java/org/una/municipalidad/services/IContribuyenteService.java
@@ -0,0 +1,35 @@
+package org.una.municipalidad.services;
+
+
+import org.una.municipalidad.dto.CobroGeneradoDTO;
+import org.una.municipalidad.dto.ContribuyenteDTO;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface IContribuyenteService {
+
+    public Optional<List<ContribuyenteDTO>> findAll();
+
+    public Optional<ContribuyenteDTO> findById(Long id);
+
+    public Optional<List<ContribuyenteDTO>> findNombre(String nombreCompleto);
+
+    public Optional<List<ContribuyenteDTO>> findByFechaNacimientoBetween(Date startDate, Date endDate);
+
+    public Optional<List<ContribuyenteDTO>> findCorreo(String correoElectronico);
+
+    public Optional<List<ContribuyenteDTO>> findDireccion(String direccion);
+
+    public Optional<List<ContribuyenteDTO>> findTelefono(String telefono);
+
+    public Optional<ContribuyenteDTO> create(ContribuyenteDTO contribuyenteDTO);
+
+    public Optional<ContribuyenteDTO> update(ContribuyenteDTO contribuyenteDTO, Long id);
+
+    public void delete(Long id);
+
+    public void deleteAll();
+
+}
