67d7e181eaaeda79fb0bbcdf3632fc24bc6fa704
JIMMM1212 <juan.morales.mora@est.una.ac.cr>
2021-10-10 10:43:21 -0600

    Correcciones Dtos

--- api/municipalidad/src/main/java/org/una/municipalidad/dto/CobroCanceladoDTO.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/dto/CobroCanceladoDTO.java
@@ -3,6 +3,7 @@ package org.una.municipalidad.dto;
 import lombok.*;
 import org.una.municipalidad.entities.CobroGenerado;
 import org.una.municipalidad.entities.Recibo;
+import org.una.municipalidad.dto.ContribuyenteDTO;
 
 import java.util.Date;
 
