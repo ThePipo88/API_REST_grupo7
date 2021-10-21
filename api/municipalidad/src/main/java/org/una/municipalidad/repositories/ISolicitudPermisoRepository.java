b2479d59a337459c32d62853f50c90bad2a2904c
JIMMM1212 <juan.morales.mora@est.una.ac.cr>
2021-10-21 13:56:07 -0600

    Merge branch 'main' into JUAN-ISAI

--- api/municipalidad/src/main/java/org/una/municipalidad/repositories/ISolicitudPermisoRepository.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/repositories/ISolicitudPermisoRepository.java
@@ -1,4 +1,17 @@
 package org.una.municipalidad.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.una.municipalidad.entities.Rol;
+import org.una.municipalidad.entities.SolicitudPermiso;
 
-public interface ISolicitudPermisoRepository {
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface ISolicitudPermisoRepository extends JpaRepository<SolicitudPermiso,Long>{
+
+
+    public List<Rol> findByFechaCreacionBetween(Date startDate, Date endDate);
+
+
 }
--- api/municipalidad/src/main/java/org/una/municipalidad/services/SolicitudPermisoServiceImplementation.java
+++ api/municipalidad/src/main/java/org/una/municipalidad/services/SolicitudPermisoServiceImplementation.java
@@ -1,4 +1,81 @@
 package org.una.municipalidad.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.una.municipalidad.dto.RolDTO;
+import org.una.municipalidad.dto.SolicitudPermisoDTO;
+import org.una.municipalidad.entities.Rol;
+import org.una.municipalidad.entities.SolicitudPermiso;
+import org.una.municipalidad.exceptions.NotFoundInformationException;
+import org.una.municipalidad.repositories.IRolRepository;
+import org.una.municipalidad.repositories.ISolicitudPermisoRepository;
+import org.una.municipalidad.utils.MapperUtils;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
-public class SolicitudPermisoServiceImplementation {
+@Service
+
+public class SolicitudPermisoServiceImplementation implements  ISolicitudPermisoService {
+    @Autowired
+    private ISolicitudPermisoRepository solicitudPermisoRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<SolicitudPermisoDTO> findById(Long id) {
+        Optional<SolicitudPermiso> rol = solicitudPermisoRepository.findById(id);
+        if (SolicitudPermiso.isEmpty()) throw new NotFoundInformationException();
+        SolicitudPermisoDTO solicitudPermisoDTO = MapperUtils.DtoFromEntity(SolicitudPermiso.get(), SolicitudPermisoDTO.class);
+        return Optional.ofNullable(solicitudPermisoDTO);
+    }
+
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<List<SolicitudPermisoDTO>> findByFechaCreacionBetween(Date startDate, Date endDate) {
+        List<SolicitudPermiso> solicitudPermisoList = solicitudPermisoRepository.findByFechaCreacionBetween(startDate,endDate);
+        List<SolicitudPermisoDTO> solicitudPermisoDTOList = MapperUtils.DtoListFromEntityList(solicitudPermisoList, SolicitudPermisoDTO.class);
+        return Optional.ofNullable(solicitudPermisoDTOList);
+    }
+
+    @Override
+    public SolicitudPermisoDTO create(SolicitudPermisoDTO solicitudPermisoDTO) {
+        return null;
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<List<SolicitudPermisoDTO>> findByEstado(boolean estado) {
+        List<SolicitudPermiso> solicitudPermisoList = solicitudPermisoRepository.findByEstado(estado);
+        List<SolicitudPermisoDTO> SolicitudPermisoDTOList = MapperUtils.DtoListFromEntityList(solicitudPermisoList, SolicitudPermisoDTO.class);
+        return Optional.ofNullable(SolicitudPermisoDTOList);
+    }
+
+    private SolicitudPermisoDTO getSavedSolicitudPermiso(SolicitudPermisoDTO solicitudPermisoDTO) {
+        SolicitudPermiso solicitudPermiso = MapperUtils.EntityFromDto(solicitudPermisoDTO, SolicitudPermiso.class);
+        SolicitudPermiso solicitudPermisoCreated = solicitudPermisoRepository.save(solicitudPermiso);
+        return MapperUtils.DtoFromEntity(solicitudPermisoCreated, SolicitudPermisoDTO.class);
+    }
+
+    @Override
+    @Transactional
+    public Optional<SolicitudPermisoDTO> update(SolicitudPermisoDTO solicitudPermisoDTO, Long id) {
+        if (solicitudPermisoRepository.findById(id).isEmpty()) throw new NotFoundInformationException();
+        return Optional.ofNullable(getSavedSolicitudPermiso(solicitudPermisoDTO));
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        solicitudPermisoRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAll() {
+        solicitudPermisoRepository.deleteAll();
+    }
+
 }
